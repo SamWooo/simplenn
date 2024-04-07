2e0d40e0d6b976d49236854e267c5cbe1997a5ac
SamWooo <wooosir@hotmail.com>
2024-04-07 16:33:01 +0800

    test img

--- test_open_clip.py
+++ test_open_clip.py
@@ -1,39 +1,13 @@
-"""import open_clip
-import torch
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-clip_model_name = "ViT-L-14"
-clip_model,_,clip_preprocess = open_clip.create_model_and_transforms(clip_model_name,pretrained = "openai",precision='fp16' if device == 'cuda' else 'fp32',device=device)
-
-tokenize = open_clip.get_tokenizer(clip_model_name)
-
-
-def image_to_features(image: Image.Image) -> torch.Tensor:
-    images = clip_preprocess(image).unsqueeze(0).to(device)
-    with torch.no_grad(), torch.cuda.amp.autocast():
-        image_features = clip_model.encode_image(images)
-    return image_features
-
-
-img = cv.imread("/path/to/example.png")
-img = Image.fromarray(img)
-
-image_feature = image_to_features(img)
-print(image_feature.shape)
-
-prompt = "a photo of a cat"
-text_tokens = tokenize([prompt]).to(device)
-text_features = clip_model.encode_text(text_tokens)
-print(text_features.shape)
-"""
 import torch
 from PIL import Image
 import open_clip
 
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
-tokenizer = open_clip.get_tokenizer('ViT-B-32')
+clip_model_name = "ViT-L-14" #'ViT-B-32'
+pretrained_data = "openai" #'laion2b_s34b_b79k'
+model, _, preprocess = open_clip.create_model_and_transforms(clip_model_name, pretrained=pretrained_data)
+tokenizer = open_clip.get_tokenizer(clip_model_name)
 
-image = preprocess(Image.open("docs/CLIP.png")).unsqueeze(0)
+image = preprocess(Image.open("./cat.jpeg")).unsqueeze(0)
 text = tokenizer(["a diagram", "a dog", "a cat"])
 
 with torch.no_grad(), torch.cuda.amp.autocast():
